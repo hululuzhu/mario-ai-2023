b0f2be7a3159c4f6a9c049035a9aa12969542fee
Quantum <hululu.zhu@gmail.com>
2023-08-09 10:02:28 -0700

    Update more logic to simplify visualization

--- lib/game_visual.py
+++ lib/game_visual.py
@@ -1,17 +1,45 @@
 """Everything related to visualization in colab notebook."""
-from PIL import Image
+from PIL import Image, ImageOps
+import game_control
 import game_env
+import os
 from ipywidgets import widgets, HBox
+from tqdm.notebook import tqdm
 
 
-def glance_env(env, gif_path, sample_steps=40, action=0):
+_GIF_PATH = "/mario_gifs"
+
+def visualize_multi_stages(start_end_worlds, start_end_stages, is_human_view=True):
+    if not os.path.exists(_GIF_PATH):
+        os.mkdir(_GIF_PATH)
+    """Runs multiple stages and returns horizontal visual boxes for each world."""
+    assert len(start_end_worlds) == len(start_end_stages) == 2, (
+        "must be pairs for worlds and stages")
+    hboxs = []
+    for w in tqdm(range(start_end_worlds)):
+        stage_names = [game_env.get_env_name(w, s) for s in range(start_end_stages)]
+        stage_names = [k[:-1] + '1' for k in stage_names]
+        multi_env = game_env.ShuffleEnv(
+           stage_names, game_control.SIMPLE_ACTIONS, is_human_view=is_human_view)
+        for i in range(start_end_stages):
+            glance_env(multi_env, f"{_GIF_PATH}/glance{w}{i}.gif",
+                       is_human_view=is_human_view)
+        hboxs.append(wrapper_gifs_horizontal([
+           f"{_GIF_PATH}/glance{w}{i}.gif" for i in range(start_end_stages)]))
+    return hboxs
+
+
+def glance_env(env, gif_path, is_human_view=True, sample_steps=40, action=0):
     """Take a glance of the world and save as gif."""
     images = []
     obs = env.reset()
     done = False
     for i in range(sample_steps):
         screen = env.render(mode='rgb_array')
-        images.append(Image.fromarray(screen))
+        if is_human_view:
+          images.append(Image.fromarray(screen))
+        else:
+          images.append(_cast_to_ai_view(screen))
         for _ in range(4): # skip frames for efficiency
             if done:
                 try:
@@ -26,6 +54,12 @@ def glance_env(env, gif_path, sample_steps=40, action=0):
             gif_path, save_all=True, append_images=images[1:], loop=0, duration=1)
     else:
         raise("Bad environment, cannot move")
+
+
+def _cast_to_ai_view(screen_frame):
+    im = Image.fromarray(screen_frame)
+    im = im.resize((84, 84,))
+    return ImageOps.grayscale(im)
 
 
 def wrapper_gifs_horizontal(gift_list):
